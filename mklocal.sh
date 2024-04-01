5796150deb23b8ea2857d91c607b7fccbee51e28
Allan Kent <allan.kent@elerian.ai>
2024-04-01 13:55:33 +0200

    adds instruction to comment out wezterm for Darwin

--- mklocal.sh
+++ mklocal.sh
@@ -12,6 +12,7 @@ if [ $OS == "Darwin" ]; then
 	sed -i '' "s/<SYSTEM>/$SYSTEM/g" flake.nix
 	sed -i '' "s/<USER>/$USER/g" home.nix
 	sed -i '' "s/<HOME>/$HOME_SAFE/g" home.nix
+	sed -i '' "/wezterm/s/^/#/" home.nix
 else
 	sed -i "s/<USER>/$USER/g" flake.nix
 	sed -i "s/<SYSTEM>/$SYSTEM/g" flake.nix
