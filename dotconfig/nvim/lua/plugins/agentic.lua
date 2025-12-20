0e27b422b74b27c0c29a56792143df658f3c3212
allank <allan.kent@gmail.com>
2025-12-20 11:03:22 +0200

    Update agentic key combos

--- dotconfig/nvim/lua/plugins/agentic.lua
+++ dotconfig/nvim/lua/plugins/agentic.lua
@@ -4,5 +4,11 @@ return {
 		"carlos-algms/agentic.nvim",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		opts = {},
+		keys = {
+			{ "<leader>aa", function() require("agentic").toggle() end, desc = "Toggle Agentic Chat" },
+			{ "<leader>as", function() require("agentic").add_selection() end, mode = "v", desc = "Add selection to Agentic" },
+			{ "<leader>af", function() require("agentic").add_file() end, desc = "Add file to Agentic" },
+			{ "<leader>an", function() require("agentic").new_session() end, desc = "New Agentic session" },
+		},
 	},
 }
