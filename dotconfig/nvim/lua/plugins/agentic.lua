0b7c7ef1bdad32f9e329f28475ec7f1cd61ae485
allank <allan.kent@gmail.com>
2025-12-20 11:41:49 +0200

    Update ACP to use gemini

--- dotconfig/nvim/lua/plugins/agentic.lua
+++ dotconfig/nvim/lua/plugins/agentic.lua
@@ -3,7 +3,9 @@ return {
 	{
 		"carlos-algms/agentic.nvim",
 		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = {},
+		opts = {
+			provider = "gemini-acp",
+		},
 		keys = {
 			{ "<leader>aa", function() require("agentic").toggle() end, desc = "Toggle Agentic Chat" },
 			{ "<leader>as", function() require("agentic").add_selection() end, mode = "v", desc = "Add selection to Agentic" },
