668a4c3b3b3415e82d20ddf1d4950e104e2473c5
allank <allan.kent@gmail.com>
2025-12-22 08:01:19 +0200

    debug LSP

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,16 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
+-- DEBUG: Mock vim.ui.select to find what's calling it with empty items
+-- See: https://github.com/folke/snacks.nvim/discussions/2264
+-- Remove this after debugging is complete
+vim.schedule(function()
+	local orig_select = vim.ui.select -- NOTE: Snacks.picker.select
+	vim.ui.select = function(items, opts, on_choice) ---@diagnostic disable-line: duplicate-set-field
+		if not items or vim.tbl_isempty(items) then
+			-- The error will create a stack trace
+			error("Force an error")
+		end
+		return orig_select(items, opts, on_choice)
+	end
+end)
